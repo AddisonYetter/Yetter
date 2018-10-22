b29b8f6d8710672930957888dd2f8ce7e775bd3a
ajy10@students.ptcollege.edu <>
2018-10-22 15:23:55 -0400

    my first commit

---
+++ src/yetter/Yetter.java
@@ -0,0 +1,10 @@
+package yetter;
+public class Yetter {
+    public static void main(String[] args) {
+        yetter();
+    }
+    public static void yetter()
+    {
+        System.out.println("Addison Yetter");
+    }
+}
